f9b9bfa0c4080d44efae50c5036778a6919a2132
Mirolim Mirzakhmedov <mirolim777@gmail.com>
2013-08-30 08:10:45 +0500

    Not displaying anything but no js errors

--- js/robotsapp.js
+++ js/robotsapp.js
@@ -1,7 +1,31 @@
-var Robots = can.Model.extend({
+
+// Represent Data using Models
+
+var Robot = can.Model.extend({
     findAll: 'GET /api/robots',
     findOne: 'GET /api/robots/{id}',
     create:  'POST /api/robots',
     update:  'PUT /api/robots/{id}',
     destroy: 'DELETE /api/robots/{id}'
 }, {});
+
+//  Managing robots
+
+Robots = can.Control({
+    init: function(){
+        this.element.html(can.view('views/robotsList.ejs', {
+            robots: this.options.robots
+        }));
+    }
+});
+
+// Bootstrapping the App
+
+$(document).ready(function(){
+    $.when(Robot.findAll()).then(
+        function(robotResponse){
+            var robots = robotResponse[0];
+            new Robots('#robots', {robots: robots});
+        }
+    );
+});
